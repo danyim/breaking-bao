12d2103b927bb242c11ee51d0d48d15fb7b4cef1
Daniel Yim <danielyim@gmail.com>
2017-03-10 20:31:59 -0800

    adding example file

---
+++ tools/config.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Breaking Bao',
+  url: 'http://breakingbao.com',
+  project: 'breaking-bao',
+  trackingID: 'UA-93488315-1',
+  ftp: {
+    host: 'breakingbao.com', // Fill in here
+    port: 21,
+    username: '', // Fill in here
+    password: '', // Fill in here
+    remoteRoot: '/public_html',
+    exclude: ['.git'],
+  },
+};
