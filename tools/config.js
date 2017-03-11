14354a2d5cd3dc2d9977806d81ba507b161ff33f
Daniel Yim <danielyim@gmail.com>
2017-03-10 20:21:04 -0800

    adding ftp deploy step

--- tools/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  title: 'Breaking Bao',        // Your website title
-  url: 'http://breakingbao.com',          // Your website URL
-  project: 'breaking-bao',      // Firebase project. See README.md -> How to Deploy
-  trackingID: 'UA-93488315-1',                 // Google Analytics Site's ID
-};
--- tools/publish.js
+++ tools/publish.js
@@ -1,15 +1,33 @@
 const path = require('path');
-const firebase = require('firebase-tools');
+// const firebase = require('firebase-tools');
+const ftp = require('ftp-deploy');
 const build = require('./build');
 const task = require('./task');
 const config = require('./config');
 
-// Build and deploy the app to Firebase
+// // Build and deploy the app to Firebase
+// module.exports = task('deploy', () => Promise.resolve()
+//   .then(() => build())
+//   .then(() => firebase.login({ nonInteractive: false }))
+//   .then(() => firebase.deploy({
+//     project: config.project,
+//     cwd: path.resolve(__dirname, '../'),
+//   }))
+//   .then(() => { setTimeout(() => process.exit()); }));
+
+const ftpConfig = {
+  username: config.ftp.username,
+  password: config.ftp.password,
+  host: config.ftp.host,
+  port: config.ftp.port,
+  localRoot: path.resolve(__dirname, '../'),
+  remoteRoot: config.ftp.remoteRoot,
+};
+
 module.exports = task('deploy', () => Promise.resolve()
   .then(() => build())
-  .then(() => firebase.login({ nonInteractive: false }))
-  .then(() => firebase.deploy({
-    project: config.project,
-    cwd: path.resolve(__dirname, '../'),
+  .then(() => ftp.deploy(ftpConfig, (err) => {
+    if (err) console.log(err);
+    else console.log('finished');
   }))
   .then(() => { setTimeout(() => process.exit()); }));
