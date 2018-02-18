0f48bb10c6fa257b758e50a7cb490233c36d0ad5
Daniel Yim <danielyim@gmail.com>
2018-02-18 03:28:12 -0800

    adding icons on footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -4,7 +4,16 @@ import s from './Footer.css'
 function Footer() {
   return (
     <footer className={s.transparent}>
-      <div className={s.logo}>
+      <div className={s.links}>
+        @breakingbaotx
+        <a href="">
+          ig <i className="icons icon-instagram" />
+        </a>
+        <a href="">fb</a>
+        <a href="">tw</a>
+        <a href="">snap</a>
+      </div>
+      <div className={s.madeByText}>
         made with &lt;3 in htx &copy; {new Date().getFullYear()} by{' '}
         <a href="http://isomorph.co" rel="noopener noreferrer" target="_blank">
           isomorph labs
