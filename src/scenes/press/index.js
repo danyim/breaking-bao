0274401bf2f6e6df0babc3b9b563635f0c3ba416
Daniel Yim <danielyim@gmail.com>
2019-10-28 13:25:43 -0700

    Add food hall articles

--- src/scenes/press/index.js
+++ src/scenes/press/index.js
@@ -15,9 +15,61 @@ const Container = styled(Layout)`
   }
 `
 
+const Flex = styled.div`
+  display: flex;
+  width: 100%;
+
+  ${({ justifyCenter }) => (justifyCenter ? 'justify-content: center;' : '')}
+  ${({ margin }) => (margin ? `margin: ${margin};` : '')}
+
+  & > div:first-child {
+    flex-basis: 350px;
+  }
+
+  & > div {
+    flex: 1 1 auto;
+  }
+`
+
 const PressPage = () => (
   <Container title="Press">
-    <div>
+    <Flex>
+      <div>
+        <a
+          href="http://houston.culturemap.com/news/restaurants-bars/08-21-19-politan-row-rice-village-food-hall-vendor-announcement-breaking-bao-cochinita-co-susu-kopi-boba/"
+          target="_blank"
+        >
+          <img
+            src="http://api.breakingbao.com/wp-content/uploads/2019/10/cmap.svg_-e1572293172766.png"
+            width="275"
+          />
+        </a>
+      </div>
+      <div>
+        <h2>Tacos, bao, and Indonesian ice cream are on deck for Rice Village's new food hall</h2>
+      </div>
+    </Flex>
+
+    <Flex>
+      <div>
+        <a
+          href="https://houston.eater.com/2019/8/21/20826710/politan-row-breaking-bao-cochinita-and-co-bar-politan-susu-kopi-and-boba-opening"
+          target="_blank"
+        >
+          <img
+            src="http://api.breakingbao.com/wp-content/uploads/2019/10/eater.svg_-e1572293270643.png"
+            width="200"
+          />
+        </a>
+      </div>
+      <div>
+        <h2>
+          Here’s More Details On What’s Opening Inside Rice Village’s Much-Anticipated Food Hall
+        </h2>
+      </div>
+    </Flex>
+
+    <Flex justifyCenter margin="25px 0 0">
       <iframe
         className="video"
         src="https://www.youtube.com/embed/y8N1hNdPW-Q"
@@ -26,31 +78,7 @@ const PressPage = () => (
         allowFullScreen
         title="Great Day Houston"
       />
-    </div>
-    <div>
-      <a
-        href="http://houston.culturemap.com/news/restaurants-bars/08-21-19-politan-row-rice-village-food-hall-vendor-announcement-breaking-bao-cochinita-co-susu-kopi-boba/"
-        target="_blank"
-      >
-        <p>Tacos, bao, and Indonesian ice cream are on deck for Rice Village's new food hall</p>
-        <img
-          src="http://api.breakingbao.com/wp-content/uploads/2019/10/cmap.svg_-e1572293172766.png"
-          width="300"
-        />
-      </a>
-    </div>
-
-    <div>
-      <a
-        href="https://houston.eater.com/2019/8/21/20826710/politan-row-breaking-bao-cochinita-and-co-bar-politan-susu-kopi-and-boba-opening"
-        target="_blank"
-      >
-        <img
-          src="http://api.breakingbao.com/wp-content/uploads/2019/10/eater.svg_-e1572293270643.png"
-          width="250"
-        />
-      </a>
-    </div>
+    </Flex>
   </Container>
 )
 
