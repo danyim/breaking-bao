908c387faa6dbffbc9dcd568c1e43e1cbd0acebb
Daniel Yim <danielyim@gmail.com>
2019-10-28 13:08:56 -0700

    adding more press pieces

--- src/scenes/press/index.js
+++ src/scenes/press/index.js
@@ -17,14 +17,40 @@ const Container = styled(Layout)`
 
 const PressPage = () => (
   <Container title="Press">
-    <iframe
-      className="video"
-      src="https://www.youtube.com/embed/y8N1hNdPW-Q"
-      frameBorder="0"
-      allow="autoPlay; encrypted-media"
-      allowFullScreen
-      title="Great Day Houston"
-    />
+    <div>
+      <iframe
+        className="video"
+        src="https://www.youtube.com/embed/y8N1hNdPW-Q"
+        frameBorder="0"
+        allow="autoPlay; encrypted-media"
+        allowFullScreen
+        title="Great Day Houston"
+      />
+    </div>
+    <div>
+      <a
+        href="http://houston.culturemap.com/news/restaurants-bars/08-21-19-politan-row-rice-village-food-hall-vendor-announcement-breaking-bao-cochinita-co-susu-kopi-boba/"
+        target="_blank"
+      >
+        <p>Tacos, bao, and Indonesian ice cream are on deck for Rice Village's new food hall</p>
+        <img
+          src="http://api.breakingbao.com/wp-content/uploads/2019/10/cmap.svg_-e1572293172766.png"
+          width="300"
+        />
+      </a>
+    </div>
+
+    <div>
+      <a
+        href="https://houston.eater.com/2019/8/21/20826710/politan-row-breaking-bao-cochinita-and-co-bar-politan-susu-kopi-and-boba-opening"
+        target="_blank"
+      >
+        <img
+          src="http://api.breakingbao.com/wp-content/uploads/2019/10/eater.svg_-e1572293270643.png"
+          width="250"
+        />
+      </a>
+    </div>
   </Container>
 )
 
